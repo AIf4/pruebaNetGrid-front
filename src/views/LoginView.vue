ea666bbc3f52f179a58ff268ecd713f484bd5958
aif4 <rutbak@gmail.com>
2023-10-06 19:15:38 -0500

    redesing

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="content-center">
-        <LoginComponent />
+    <div class="h-screen">
+
+        <div class="dark:bg-slate-900 bg-gray-100 flex h-full items-center py-16">
+            <LoginComponent />
+        </div>
+
     </div>
-    
 </template>
 
 <script lang="ts">
 import LoginComponent from '@/components/LoginComponent.vue';
 
+
 export default {
     components: {
-        LoginComponent, 
+        LoginComponent,
     },
 };
 </script>
